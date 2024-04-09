4374454652aa570620af8dafc4ff2d283b09fd3d
Ted <tedzolotarev@gmail.com>
2024-04-09 16:24:55 +0300

    Fix image property

--- src/utils.jl
+++ src/utils.jl
@@ -1,10 +1,9 @@
 unzip(a) = map(x->getfield.(a, x), fieldnames(eltype(a)))
 
 function bbox_to_view(box, img, preserve_indices=false)
-    axes = Base.splat(range).(box |> unzip)
-    view_box = @view img[axes...]
+    view_box = @view img[box]
     if preserve_indices
-        return OffsetArray(view_box, axes...)
+        return OffsetArray(view_box, box)
     else
         return view_box    
     end
